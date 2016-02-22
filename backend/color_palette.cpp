jorgen/yat

commit dbf0ca2bc1ebde47900704e0c4d27c38b742d9cb

store colors as QRgb rather than QColor to save a little space

backend/color_palette.cpp
@@ -2,247 +2,247 @@
 
 #include <QDebug>
 
-QVector<QColor> ColorPalette::m_xtermColors {
-    QColor(0,0,0), // index 16
-    QColor(0,0,95),
-    QColor(0,0,135),
-    QColor(0,0,175),
-    QColor(0,0,215),
-    QColor(0,0,255),
-    QColor(0,95,0),
-    QColor(0,95,95),
-    QColor(0,95,135),
-    QColor(0,95,175),
-    QColor(0,95,215),
-    QColor(0,95,255),
-    QColor(0,135,0),
-    QColor(0,135,95),
-    QColor(0,135,135),
-    QColor(0,135,175),
-    QColor(0,135,215),
-    QColor(0,135,255),
-    QColor(0,175,0),
-    QColor(0,175,95),
-    QColor(0,175,135),
-    QColor(0,175,175),
-    QColor(0,175,215),
-    QColor(0,175,255),
-    QColor(0,215,0),
-    QColor(0,215,95),
-    QColor(0,215,135),
-    QColor(0,215,175),
-    QColor(0,215,215),
-    QColor(0,215,255),
-    QColor(0,255,0),
-    QColor(0,255,95),
-    QColor(0,255,135),
-    QColor(0,255,175),
-    QColor(0,255,215),
-    QColor(0,255,255),
-    QColor(95,0,0),
-    QColor(95,0,95),
-    QColor(95,0,135),
-    QColor(95,0,175),
-    QColor(95,0,215),
-    QColor(95,0,255),
-    QColor(95,95,0),
-    QColor(95,95,95),
-    QColor(95,95,135),
-    QColor(95,95,175),
-    QColor(95,95,215),
-    QColor(95,95,255),
-    QColor(95,135,0),
-    QColor(95,135,95),
-    QColor(95,135,135),
-    QColor(95,135,175),
-    QColor(95,135,215),
-    QColor(95,135,255),
-    QColor(95,175,0),
-    QColor(95,175,95),
-    QColor(95,175,135),
-    QColor(95,175,175),
-    QColor(95,175,215),
-    QColor(95,175,255),
-    QColor(95,215,0),
-    QColor(95,215,95),
-    QColor(95,215,135),
-    QColor(95,215,175),
-    QColor(95,215,215),
-    QColor(95,215,255),
-    QColor(95,255,0),
-    QColor(95,255,95),
-    QColor(95,255,135),
-    QColor(95,255,175),
-    QColor(95,255,215),
-    QColor(95,255,255),
-    QColor(135,0,0),
-    QColor(135,0,95),
-    QColor(135,0,135),
-    QColor(135,0,175),
-    QColor(135,0,215),
-    QColor(135,0,255),
-    QColor(135,95,0),
-    QColor(135,95,95),
-    QColor(135,95,135),
-    QColor(135,95,175),
-    QColor(135,95,215),
-    QColor(135,95,255),
-    QColor(135,135,0),
-    QColor(135,135,95),
-    QColor(135,135,135),
-    QColor(135,135,175),
-    QColor(135,135,215),
-    QColor(135,135,255),
-    QColor(135,175,0),
-    QColor(135,175,95),
-    QColor(135,175,135),
-    QColor(135,175,175),
-    QColor(135,175,215),
-    QColor(135,175,255),
-    QColor(135,215,0),
-    QColor(135,215,95),
-    QColor(135,215,135),
-    QColor(135,215,175),
-    QColor(135,215,215),
-    QColor(135,215,255),
-    QColor(135,255,0),
-    QColor(135,255,95),
-    QColor(135,255,135),
-    QColor(135,255,175),
-    QColor(135,255,215),
-    QColor(135,255,255),
-    QColor(175,0,0),
-    QColor(175,0,95),
-    QColor(175,0,135),
-    QColor(175,0,175),
-    QColor(175,0,215),
-    QColor(175,0,255),
-    QColor(175,95,0),
-    QColor(175,95,95),
-    QColor(175,95,135),
-    QColor(175,95,175),
-    QColor(175,95,215),
-    QColor(175,95,255),
-    QColor(175,135,0),
-    QColor(175,135,95),
-    QColor(175,135,135),
-    QColor(175,135,175),
-    QColor(175,135,215),
-    QColor(175,135,255),
-    QColor(175,175,0),
-    QColor(175,175,95),
-    QColor(175,175,135),
-    QColor(175,175,175),
-    QColor(175,175,215),
-    QColor(175,175,255),
-    QColor(175,215,0),
-    QColor(175,215,95),
-    QColor(175,215,135),
-    QColor(175,215,175),
-    QColor(175,215,215),
-    QColor(175,215,255),
-    QColor(175,255,0),
-    QColor(175,255,95),
-    QColor(175,255,135),
-    QColor(175,255,175),
-    QColor(175,255,215),
-    QColor(175,255,255),
-    QColor(215,0,0),
-    QColor(215,0,95),
-    QColor(215,0,135),
-    QColor(215,0,175),
-    QColor(215,0,215),
-    QColor(215,0,255),
-    QColor(215,95,0),
-    QColor(215,95,95),
-    QColor(215,95,135),
-    QColor(215,95,175),
-    QColor(215,95,215),
-    QColor(215,95,255),
-    QColor(215,135,0),
-    QColor(215,135,95),
-    QColor(215,135,135),
-    QColor(215,135,175),
-    QColor(215,135,215),
-    QColor(215,135,255),
-    QColor(215,175,0),
-    QColor(215,175,95),
-    QColor(215,175,135),
-    QColor(215,175,175),
-    QColor(215,175,215),
-    QColor(215,175,255),
-    QColor(215,215,0),
-    QColor(215,215,95),
-    QColor(215,215,135),
-    QColor(215,215,175),
-    QColor(215,215,215),
-    QColor(215,215,255),
-    QColor(215,255,0),
-    QColor(215,255,95),
-    QColor(215,255,135),
-    QColor(215,255,175),
-    QColor(215,255,215),
-    QColor(215,255,255),
-    QColor(255,0,0),
-    QColor(255,0,95),
-    QColor(255,0,135),
-    QColor(255,0,175),
-    QColor(255,0,215),
-    QColor(255,0,255),
-    QColor(255,95,0),
-    QColor(255,95,95),
-    QColor(255,95,135),
-    QColor(255,95,175),
-    QColor(255,95,215),
-    QColor(255,95,255),
-    QColor(255,135,0),
-    QColor(255,135,95),
-    QColor(255,135,135),
-    QColor(255,135,175),
-    QColor(255,135,215),
-    QColor(255,135,255),
-    QColor(255,175,0),
-    QColor(255,175,95),
-    QColor(255,175,135),
-    QColor(255,175,175),
-    QColor(255,175,215),
-    QColor(255,175,255),
-    QColor(255,215,0),
-    QColor(255,215,95),
-    QColor(255,215,135),
-    QColor(255,215,175),
-    QColor(255,215,215),
-    QColor(255,215,255),
-    QColor(255,255,0),
-    QColor(255,255,95),
-    QColor(255,255,135),
-    QColor(255,255,175),
-    QColor(255,255,215),
-    QColor(255,255,255),
-    QColor(8,8,8),
-    QColor(18,18,18),
-    QColor(28,28,28),
-    QColor(38,38,38),
-    QColor(48,48,48),
-    QColor(58,58,58),
-    QColor(68,68,68),
-    QColor(78,78,78),
-    QColor(88,88,88),
-    QColor(98,98,98),
-    QColor(108,108,108),
-    QColor(118,118,118),
-    QColor(128,128,128),
-    QColor(138,138,138),
-    QColor(148,148,148),
-    QColor(158,158,158),
-    QColor(168,168,168),
-    QColor(178,178,178),
-    QColor(188,188,188),
-    QColor(198,198,198),
-    QColor(208,208,208),
-    QColor(218,218,218),
-    QColor(228,228,228),
-    QColor(238,238,238) // index 255
+QVector<QRgb> ColorPalette::m_xtermColors {
+    QColor(0,0,0).rgb(),    // index 16
+    QColor(0,0,95).rgb(),
+    QColor(0,0,135).rgb(),
+    QColor(0,0,175).rgb(),
+    QColor(0,0,215).rgb(),
+    QColor(0,0,255).rgb(),
+    QColor(0,95,0).rgb(),
+    QColor(0,95,95).rgb(),
+    QColor(0,95,135).rgb(),
+    QColor(0,95,175).rgb(),
+    QColor(0,95,215).rgb(),
+    QColor(0,95,255).rgb(),
+    QColor(0,135,0).rgb(),
+    QColor(0,135,95).rgb(),
+    QColor(0,135,135).rgb(),
+    QColor(0,135,175).rgb(),
+    QColor(0,135,215).rgb(),
+    QColor(0,135,255).rgb(),
+    QColor(0,175,0).rgb(),
+    QColor(0,175,95).rgb(),
+    QColor(0,175,135).rgb(),
+    QColor(0,175,175).rgb(),
+    QColor(0,175,215).rgb(),
+    QColor(0,175,255).rgb(),
+    QColor(0,215,0).rgb(),
+    QColor(0,215,95).rgb(),
+    QColor(0,215,135).rgb(),
+    QColor(0,215,175).rgb(),
+    QColor(0,215,215).rgb(),
+    QColor(0,215,255).rgb(),
+    QColor(0,255,0).rgb(),
+    QColor(0,255,95).rgb(),
+    QColor(0,255,135).rgb(),
+    QColor(0,255,175).rgb(),
+    QColor(0,255,215).rgb(),
+    QColor(0,255,255).rgb(),
+    QColor(95,0,0).rgb(),
+    QColor(95,0,95).rgb(),
+    QColor(95,0,135).rgb(),
+    QColor(95,0,175).rgb(),
+    QColor(95,0,215).rgb(),
+    QColor(95,0,255).rgb(),
+    QColor(95,95,0).rgb(),
+    QColor(95,95,95).rgb(),
+    QColor(95,95,135).rgb(),
+    QColor(95,95,175).rgb(),
+    QColor(95,95,215).rgb(),
+    QColor(95,95,255).rgb(),
+    QColor(95,135,0).rgb(),
+    QColor(95,135,95).rgb(),
+    QColor(95,135,135).rgb(),
+    QColor(95,135,175).rgb(),
+    QColor(95,135,215).rgb(),
+    QColor(95,135,255).rgb(),
+    QColor(95,175,0).rgb(),
+    QColor(95,175,95).rgb(),
+    QColor(95,175,135).rgb(),
+    QColor(95,175,175).rgb(),
+    QColor(95,175,215).rgb(),
+    QColor(95,175,255).rgb(),
+    QColor(95,215,0).rgb(),
+    QColor(95,215,95).rgb(),
+    QColor(95,215,135).rgb(),
+    QColor(95,215,175).rgb(),
+    QColor(95,215,215).rgb(),
+    QColor(95,215,255).rgb(),
+    QColor(95,255,0).rgb(),
+    QColor(95,255,95).rgb(),
+    QColor(95,255,135).rgb(),
+    QColor(95,255,175).rgb(),
+    QColor(95,255,215).rgb(),
+    QColor(95,255,255).rgb(),
+    QColor(135,0,0).rgb(),
+    QColor(135,0,95).rgb(),
+    QColor(135,0,135).rgb(),
+    QColor(135,0,175).rgb(),
+    QColor(135,0,215).rgb(),
+    QColor(135,0,255).rgb(),
+    QColor(135,95,0).rgb(),
+    QColor(135,95,95).rgb(),
+    QColor(135,95,135).rgb(),
+    QColor(135,95,175).rgb(),
+    QColor(135,95,215).rgb(),
+    QColor(135,95,255).rgb(),
+    QColor(135,135,0).rgb(),
+    QColor(135,135,95).rgb(),
+    QColor(135,135,135).rgb(),
+    QColor(135,135,175).rgb(),
+    QColor(135,135,215).rgb(),
+    QColor(135,135,255).rgb(),
+    QColor(135,175,0).rgb(),
+    QColor(135,175,95).rgb(),
+    QColor(135,175,135).rgb(),
+    QColor(135,175,175).rgb(),
+    QColor(135,175,215).rgb(),
+    QColor(135,175,255).rgb(),
+    QColor(135,215,0).rgb(),
+    QColor(135,215,95).rgb(),
+    QColor(135,215,135).rgb(),
+    QColor(135,215,175).rgb(),
+    QColor(135,215,215).rgb(),
+    QColor(135,215,255).rgb(),
+    QColor(135,255,0).rgb(),
+    QColor(135,255,95).rgb(),
+    QColor(135,255,135).rgb(),
+    QColor(135,255,175).rgb(),
+    QColor(135,255,215).rgb(),
+    QColor(135,255,255).rgb(),
+    QColor(175,0,0).rgb(),
+    QColor(175,0,95).rgb(),
+    QColor(175,0,135).rgb(),
+    QColor(175,0,175).rgb(),
+    QColor(175,0,215).rgb(),
+    QColor(175,0,255).rgb(),
+    QColor(175,95,0).rgb(),
+    QColor(175,95,95).rgb(),
+    QColor(175,95,135).rgb(),
+    QColor(175,95,175).rgb(),
+    QColor(175,95,215).rgb(),
+    QColor(175,95,255).rgb(),
+    QColor(175,135,0).rgb(),
+    QColor(175,135,95).rgb(),
+    QColor(175,135,135).rgb(),
+    QColor(175,135,175).rgb(),
+    QColor(175,135,215).rgb(),
+    QColor(175,135,255).rgb(),
+    QColor(175,175,0).rgb(),
+    QColor(175,175,95).rgb(),
+    QColor(175,175,135).rgb(),
+    QColor(175,175,175).rgb(),
+    QColor(175,175,215).rgb(),
+    QColor(175,175,255).rgb(),
+    QColor(175,215,0).rgb(),
+    QColor(175,215,95).rgb(),
+    QColor(175,215,135).rgb(),
+    QColor(175,215,175).rgb(),
+    QColor(175,215,215).rgb(),
+    QColor(175,215,255).rgb(),
+    QColor(175,255,0).rgb(),
+    QColor(175,255,95).rgb(),
+    QColor(175,255,135).rgb(),
+    QColor(175,255,175).rgb(),
+    QColor(175,255,215).rgb(),
+    QColor(175,255,255).rgb(),
+    QColor(215,0,0).rgb(),
+    QColor(215,0,95).rgb(),
+    QColor(215,0,135).rgb(),
+    QColor(215,0,175).rgb(),
+    QColor(215,0,215).rgb(),
+    QColor(215,0,255).rgb(),
+    QColor(215,95,0).rgb(),
+    QColor(215,95,95).rgb(),
+    QColor(215,95,135).rgb(),
+    QColor(215,95,175).rgb(),
+    QColor(215,95,215).rgb(),
+    QColor(215,95,255).rgb(),
+    QColor(215,135,0).rgb(),
+    QColor(215,135,95).rgb(),
+    QColor(215,135,135).rgb(),
+    QColor(215,135,175).rgb(),
+    QColor(215,135,215).rgb(),
+    QColor(215,135,255).rgb(),
+    QColor(215,175,0).rgb(),
+    QColor(215,175,95).rgb(),
+    QColor(215,175,135).rgb(),
+    QColor(215,175,175).rgb(),
+    QColor(215,175,215).rgb(),
+    QColor(215,175,255).rgb(),
+    QColor(215,215,0).rgb(),
+    QColor(215,215,95).rgb(),
+    QColor(215,215,135).rgb(),
+    QColor(215,215,175).rgb(),
+    QColor(215,215,215).rgb(),
+    QColor(215,215,255).rgb(),
+    QColor(215,255,0).rgb(),
+    QColor(215,255,95).rgb(),
+    QColor(215,255,135).rgb(),
+    QColor(215,255,175).rgb(),
+    QColor(215,255,215).rgb(),
+    QColor(215,255,255).rgb(),
+    QColor(255,0,0).rgb(),
+    QColor(255,0,95).rgb(),
+    QColor(255,0,135).rgb(),
+    QColor(255,0,175).rgb(),
+    QColor(255,0,215).rgb(),
+    QColor(255,0,255).rgb(),
+    QColor(255,95,0).rgb(),
+    QColor(255,95,95).rgb(),
+    QColor(255,95,135).rgb(),
+    QColor(255,95,175).rgb(),
+    QColor(255,95,215).rgb(),
+    QColor(255,95,255).rgb(),
+    QColor(255,135,0).rgb(),
+    QColor(255,135,95).rgb(),
+    QColor(255,135,135).rgb(),
+    QColor(255,135,175).rgb(),
+    QColor(255,135,215).rgb(),
+    QColor(255,135,255).rgb(),
+    QColor(255,175,0).rgb(),
+    QColor(255,175,95).rgb(),
+    QColor(255,175,135).rgb(),
+    QColor(255,175,175).rgb(),
+    QColor(255,175,215).rgb(),
+    QColor(255,175,255).rgb(),
+    QColor(255,215,0).rgb(),
+    QColor(255,215,95).rgb(),
+    QColor(255,215,135).rgb(),
+    QColor(255,215,175).rgb(),
+    QColor(255,215,215).rgb(),
+    QColor(255,215,255).rgb(),
+    QColor(255,255,0).rgb(),
+    QColor(255,255,95).rgb(),
+    QColor(255,255,135).rgb(),
+    QColor(255,255,175).rgb(),
+    QColor(255,255,215).rgb(),
+    QColor(255,255,255).rgb(),
+    QColor(8,8,8).rgb(),
+    QColor(18,18,18).rgb(),
+    QColor(28,28,28).rgb(),
+    QColor(38,38,38).rgb(),
+    QColor(48,48,48).rgb(),
+    QColor(58,58,58).rgb(),
+    QColor(68,68,68).rgb(),
+    QColor(78,78,78).rgb(),
+    QColor(88,88,88).rgb(),
+    QColor(98,98,98).rgb(),
+    QColor(108,108,108).rgb(),
+    QColor(118,118,118).rgb(),
+    QColor(128,128,128).rgb(),
+    QColor(138,138,138).rgb(),
+    QColor(148,148,148).rgb(),
+    QColor(158,158,158).rgb(),
+    QColor(168,168,168).rgb(),
+    QColor(178,178,178).rgb(),
+    QColor(188,188,188).rgb(),
+    QColor(198,198,198).rgb(),
+    QColor(208,208,208).rgb(),
+    QColor(218,218,218).rgb(),
+    QColor(228,228,228).rgb(),
+    QColor(238,238,238).rgb()   // index 255
 };
 
 
@@ -302,6 +302,12 @@ QColor ColorPalette::lightColor(ColorPalette::Color color) const
 }
 
 QColor ColorPalette::xtermColor(int index)
+{
+    Q_ASSERT(index >= 16);
+    return QColor(m_xtermColors[index - 16]);
+}
+
+QRgb ColorPalette::xtermRgb(int index)
 {
     Q_ASSERT(index >= 16);
     return m_xtermColors[index - 16];

backend/color_palette.h
@@ -33,6 +33,7 @@ public:
     QColor normalColor(Color color) const;
     QColor lightColor(Color color) const;
     static QColor xtermColor(int index);
+    static QRgb xtermRgb(int index);
 
     void setInverseDefaultColors(bool inverse);
 
@@ -45,7 +46,7 @@ private:
     QVector<QColor> m_normalColors;
     QVector<QColor> m_lightColors;
     QVector<QColor> m_intenseColors;
-    static QVector<QColor> m_xtermColors;
+    static QVector<QRgb> m_xtermColors;
 
     bool m_inverse_default;
 };

backend/cursor.cpp
@@ -208,12 +208,12 @@ void Cursor::setTextStyleColor(ushort color)
     }
 }
 
-void Cursor::setTextForegroundColor(const QColor &color)
+void Cursor::setTextForegroundColor(const QRgb &color)
 {
     m_current_text_style.forground = color;
 }
 
-void Cursor::setTextBackgroundColor(const QColor &color)
+void Cursor::setTextBackgroundColor(const QRgb &color)
 {
     m_current_text_style.background = color;
 }

backend/cursor.h
@@ -58,8 +58,8 @@ public:
 
     void setTextStyleColor(ushort color);
     ColorPalette *colorPalette() const;
-    void setTextForegroundColor(const QColor &color);
-    void setTextBackgroundColor(const QColor &color);
+    void setTextForegroundColor(const QRgb &color);
+    void setTextBackgroundColor(const QRgb &color);
 
     QPoint position() const;
     int x() const;

backend/parser.cpp
@@ -1284,11 +1284,11 @@ void Parser::handleSGR()
 }
 
 void Parser::handleXtermColor(int param, int i) {
-    QColor color;
+    QRgb color;
     switch (m_parameters.at(++i)) {
         case 5:
             if (m_parameters.size() >= 3)
-                color = ColorPalette::xtermColor(m_parameters.at(++i));
+                color = ColorPalette::xtermRgb(m_parameters.at(++i));
             else
                 qDebug() << "8-bit color bytes unexpected" << m_parameters;
             break;

backend/text_style.h
@@ -29,8 +29,8 @@ public:
     TextStyle();
 
     Styles style;
-    QColor forground;
-    QColor background;
+    QRgb forground;
+    QRgb background;
 
     bool isCompatible(const TextStyle &other) const;
 };

commit a6adba70ea881c60277421e879ed013498e32e2d

always initialize foreground/background with QRgb colors
More fallout from dbf0ca2b: TextStyle.foreground and background are of type
QRgb so we can't initialize them directly to enum values; so now we need to
access the ColorPalette instance to get the default colors.
This bad initialization caused a bug: with very rapid output from the
shell (such as when running find), some lines of text would have foreground
color 8, nearly black, and fail to be changed again later, for some reason.

Also removed stuff that isn't in use: the intense color table, and
the xtermColor accessor.

backend/color_palette.cpp
@@ -2,7 +2,7 @@
 
 #include <QDebug>
 
-QVector<QRgb> ColorPalette::m_xtermColors {
+static QVector<QRgb> defaultXtermColors = {
     QColor(0,0,0).rgb(),    // index 16
     QColor(0,0,95).rgb(),
     QColor(0,0,135).rgb(),
@@ -250,7 +250,7 @@ ColorPalette::ColorPalette(QObject *parent)
     : QObject(parent)
     , m_normalColors(numberOfColors)
     , m_lightColors(numberOfColors)
-    , m_intenseColors(numberOfColors)
+    , m_xtermColors(defaultXtermColors)
     , m_inverse_default(false)
 {
     m_normalColors[0].setRgb(0,0,0);
@@ -301,10 +301,9 @@ QColor ColorPalette::lightColor(ColorPalette::Color color) const
     return this->color(color,true);
 }
 
-QColor ColorPalette::xtermColor(int index)
+QRgb ColorPalette::normalRgb(int index)
 {
-    Q_ASSERT(index >= 16);
-    return QColor(m_xtermColors[index - 16]);
+    return m_normalColors[index].rgb();
 }
 
 QRgb ColorPalette::xtermRgb(int index)
@@ -321,6 +320,11 @@ void ColorPalette::setInverseDefaultColors(bool inverse)
         emit changed();
         emit defaultBackgroundColorChanged();
     }
+}
+
+QColor ColorPalette::defaultForeground() const
+{
+    return normalColor(DefaultForeground);
 }
 
 QColor ColorPalette::defaultBackground() const

backend/color_palette.h
@@ -33,11 +33,12 @@ public:
     QColor color(Color color, bool bold = false) const;
     QColor normalColor(Color color) const;
     QColor lightColor(Color color) const;
-    static QColor xtermColor(int index);
-    static QRgb xtermRgb(int index);
+    QRgb normalRgb(int index);
+    QRgb xtermRgb(int index);
 
     void setInverseDefaultColors(bool inverse);
 
+    QColor defaultForeground() const;
     QColor defaultBackground() const;
 signals:
     void changed();
@@ -46,8 +47,7 @@ signals:
 private:
     QVector<QColor> m_normalColors;
     QVector<QColor> m_lightColors;
-    QVector<QColor> m_intenseColors;
-    static QVector<QRgb> m_xtermColors;
+    QVector<QRgb> m_xtermColors;
 
     bool m_inverse_default;
 };

backend/cursor.cpp
@@ -157,8 +157,8 @@ void Cursor::setTextStyle(TextStyle::Style style, bool add)
 
 void Cursor::resetStyle()
 {
-    m_current_text_style.background = ColorPalette::DefaultBackground;
-    m_current_text_style.foreground = ColorPalette::DefaultForeground;
+    m_current_text_style.background = colorPalette()->defaultBackground().rgb();
+    m_current_text_style.foreground = colorPalette()->defaultForeground().rgb();
     m_current_text_style.style = TextStyle::Normal;
 }
 
@@ -195,12 +195,12 @@ TextStyle Cursor::currentTextStyle() const
     return m_current_text_style;
 }
 
-void Cursor::setTextForegroundColor(const QRgb &color)
+void Cursor::setTextForegroundColor(QRgb color)
 {
     m_current_text_style.foreground = color;
 }
 
-void Cursor::setTextBackgroundColor(const QRgb &color)
+void Cursor::setTextBackgroundColor(QRgb color)
 {
     m_current_text_style.background = color;
 }

backend/cursor.h
@@ -57,8 +57,8 @@ public:
     TextStyle currentTextStyle() const;
 
     ColorPalette *colorPalette() const;
-    void setTextForegroundColor(const QRgb &color);
-    void setTextBackgroundColor(const QRgb &color);
+    void setTextForegroundColor(QRgb color);
+    void setTextBackgroundColor(QRgb color);
     void setTextForegroundColorIndex(ColorPalette::Color color);
     void setTextBackgroundColorIndex(ColorPalette::Color color);
 

backend/parser.cpp
@@ -1288,7 +1288,7 @@ void Parser::handleXtermColor(int param, int i) {
     switch (m_parameters.at(++i)) {
         case 5:
             if (m_parameters.size() >= 3)
-                color = ColorPalette::xtermRgb(m_parameters.at(++i));
+                color = m_screen->colorPalette()->xtermRgb(m_parameters.at(++i));
             else
                 qCWarning(lcParser) << "8-bit color bytes unexpected" << m_parameters;
             break;

backend/screen.cpp
@@ -199,8 +199,8 @@ TextStyle Screen::defaultTextStyle() const
 {
     TextStyle style;
     style.style = TextStyle::Normal;
-    style.foreground = ColorPalette::DefaultForeground;
-    style.background = ColorPalette::DefaultBackground;
+    style.foreground = colorPalette()->defaultForeground().rgb();
+    style.background = colorPalette()->defaultBackground().rgb();
     return style;
 }
 

backend/text.cpp
@@ -36,6 +36,8 @@ Text::Text(Screen *screen)
     , m_line(0)
     , m_old_line(0)
     , m_width(1)
+    , m_style(screen->defaultTextStyle())
+    , m_new_style(screen->defaultTextStyle())
     , m_style_dirty(true)
     , m_text_dirty(true)
     , m_visible(true)

backend/text_style.cpp
@@ -7,10 +7,9 @@
 
 TextStyle::TextStyle()
     : style(Normal)
-    , foreground(ColorPalette::DefaultForeground)
-    , background(ColorPalette::DefaultBackground)
+    , foreground(0)
+    , background(0)
 {
-
 }
 
 bool TextStyle::isCompatible(const TextStyle &other) const